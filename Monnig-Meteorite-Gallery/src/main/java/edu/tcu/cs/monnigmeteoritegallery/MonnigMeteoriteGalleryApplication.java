7191b5e078b6516663cb50bf652ca9dfafe30309
Peter Chen <p.chen@tcu.edu>
2024-03-20 15:09:39 -0500

    Generated Dependencies
    Generated dependencies based on spring boot tutorial

---
+++ Monnig-Meteorite-Gallery/src/main/java/edu/tcu/cs/monnigmeteoritegallery/MonnigMeteoriteGalleryApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.monnigmeteoritegallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonnigMeteoriteGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonnigMeteoriteGalleryApplication.class, args);
+	}
+
+}
