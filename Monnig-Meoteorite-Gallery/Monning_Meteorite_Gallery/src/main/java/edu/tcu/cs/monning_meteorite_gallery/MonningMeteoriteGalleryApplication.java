016152b37ba962da25f1c1205a32e91a77a6b285
Peter Chen <p.chen@tcu.edu>
2024-03-25 01:13:36 -0500

    Revert "Deleted old dir"
    This reverts commit cd944c9b2893f99085f8508987e0114751bc3a3f.

---
+++ Monnig-Meoteorite-Gallery/Monning_Meteorite_Gallery/src/main/java/edu/tcu/cs/monning_meteorite_gallery/MonningMeteoriteGalleryApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.monning_meteorite_gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonningMeteoriteGalleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonningMeteoriteGalleryApplication.class, args);
+    }
+
+}
---
+++ Monnig-Meoteorite-Gallery/Monning_Meteorite_Gallery/src/test/java/edu/tcu/cs/monning_meteorite_gallery/MonningMeteoriteGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.monning_meteorite_gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonningMeteoriteGalleryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
